93fe8b88e93514f27c0a875e1a8547b29322f38a
Dan Palmer <dan@danpalmer.me>
2018-09-22 18:08:27 +0100

    Add a check for structure

--- graphql_validate/checks/structure.py
+++ graphql_validate/checks/structure.py
@@ -1,8 +1,46 @@
 """
 Checks on the structure of a GraphQL schema.
 """
+from typing import List
+
+from graphql import (
+    is_input_object_type,
+    is_list_type,
+    is_non_null_type,
+    is_nullable_type,
+    is_object_type,
+)
+
+from ..logging import logger
+from .issues import TypeIssue
+
+RELAY_FIELD_LIST_NAME = "edges"
 
 
-def get_structural_issues(schema):
+def get_structural_issues(schema) -> List[TypeIssue]:
     """Gets an iterable of structural issues with a schema."""
-    pass
+
+    logger.debug("Linting structure of schema")
+
+    for type_name, type_ in schema.type_map.items():
+        if is_object_type(type_) or is_input_object_type(type_):
+            for field_name, field in type_.fields.items():
+                if field_name == RELAY_FIELD_LIST_NAME:
+                    continue
+
+                possible_list_type = field.type
+
+                if is_non_null_type(field.type):
+                    possible_list_type = field.type.of_type
+
+                if not is_list_type(possible_list_type):
+                    continue
+
+                if is_nullable_type(possible_list_type.of_type):
+                    yield TypeIssue(
+                        type_name=type_name,
+                        type_issues=[],
+                        field_issues={
+                            field_name: ["Lists should not allow nullable elements."]
+                        },
+                    )
